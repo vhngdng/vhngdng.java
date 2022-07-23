bb3f6cb2b06870377cb858e775618bac44675163
dung <vhngdng@gmail.com>
2022-07-23 16:06:36 +0700

    BVN DAY02

--- DAY02/HomeWork02.java
+++ DAY02/HomeWork02.java
@@ -2,10 +2,13 @@ package DAY02;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Formatter;
 import java.util.Scanner;
 
 import javax.sound.sampled.LineUnavailableException;
@@ -27,29 +30,30 @@ public class HomeWork02 {
         System.out.println("Khoảng cách của bạn là: " + distant + "km");
         
         // nhập thời gian bắt đầu, gán vào biên startTimeStr
-        System.out.println("Nhập thời gian bắt đầu: ");
+        System.out.println("Nhập thời gian bắt đầu (theo mẫu:dd-MM-yyyy HH:mm):  ");
         startTimeStr = scanner.nextLine();
         
         
         // nhập thời gian kết thúc, gán vào biến finishTimeStr
-        System.out.print("Nhập thời gian kết thúc: ");
+        System.out.print("Nhập thời gian kết thúc (theo mẫu dd-MM-yyyy HH:mm): ");
         finishTimeStr = scanner.nextLine();
 
         scanner.close();
         
-        /*
-        // chuyển startTimeStr và finishTimeStr sang kiểu LocalDateTime
-        LocalDateTime startTime = LocalDateTime.formater.parse(LocalDate.of(year, month, dayOfMonth), LocalTime.of(hour, minute, second));
-        LocalDateTime finishTime = LocalDateTime.parse(<code của bạn>, <code của bạn>);
         
-        DateFormat formater = new DateFormat(dd, hh, mm);
+        // chuyển startTimeStr và finishTimeStr sang kiểu LocalDateTime
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        LocalDateTime startTime = LocalDateTime.parse(startTimeStr, formatter);
+        LocalDateTime finishTime = LocalDateTime.parse(finishTimeStr, formatter);
+        
+        
 
         Duration duration = Duration.between(startTime, finishTime);
         // Tính và in ra thời gian chạy của bạn
         System.out.printf("Thời gian chạy %s phút\n", duration.toMinutes());
         // Tính và in ra vận tốc trung bình của bạn (km/h)
-        System.out.printf("Vận tốc trung bình %s km/h\n", distant / <code của bạn>);
+        System.out.printf("Vận tốc trung bình %s km/h\n", distant /duration.toHours());
     
-        */
+        
   }
 }
--- DAY02/HomeWorkMySelf.java
+++ DAY02/HomeWorkMySelf.java
@@ -12,23 +12,23 @@ import java.time.format.DateTimeFormatterBuilder;
 import java.util.Scanner;
 
 public class HomeWorkMySelf {
-  public static void main (String[] args){
+  public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    
+
     // Nhập khoảng cách:
 
-        System.out.print("Nhập khoảng cách: ");
-        double distant = scan.nextDouble();
-        scan.nextLine();
-        System.out.println("Khoảng cách của bạn là: " + distant + "km");
-    
-    /* NHap thoi gian bat dau*/
+    System.out.print("Nhập khoảng cách: ");
+    double distant = scan.nextDouble();
+    scan.nextLine();
+    System.out.println("Khoảng cách của bạn là: " + distant + "km");
+
+    /* NHap thoi gian bat dau */
 
     System.out.println("Nhập ngày bắt đầu: ");
     int day = scan.nextInt();
     String limitday = (day <= 31) && (day >= 1) ? "Ngày " + day : "Thông tin không phù hợp";
     System.out.println(limitday);
-    
+
     // Nhập tháng bắt đầu
     System.out.println("Nhập tháng bắt đầu: ");
     int month = scan.nextInt();
@@ -41,14 +41,14 @@ public class HomeWorkMySelf {
     System.out.println("Nhập giờ bắt đầu: ");
     int hour = scan.nextInt();
 
-    //Nhap phut bat dau
+    // Nhap phut bat dau
     System.out.println("Nhập phút bắt đầu: ");
     int minute = scan.nextInt();
 
-    System.out.printf("Thời gian bắt đầu của bạn là: Ngày %d, Tháng %d, Năm %d, %dh:%d'\n\n",day,month,year,hour,minute);
+    System.out.printf("Thời gian bắt đầu của bạn là: Ngày %d, Tháng %d, Năm %d, %dh:%d'\n\n", day, month, year, hour,
+        minute);
 
-
-    /* NHap thoi gian ket thuc*/
+    /* NHap thoi gian ket thuc */
 
     System.out.println("Nhập ngày kết thúc: ");
     int dayFinish = scan.nextInt();
@@ -65,32 +65,26 @@ public class HomeWorkMySelf {
     System.out.println("Nhập giờ kết thúc: ");
     int hourFinish = scan.nextInt();
 
-    //Nhap phut kết thúc
+    // Nhap phut kết thúc
     System.out.println("Nhập phút kết thúc: ");
     int minuteFinish = scan.nextInt();
 
-    System.out.printf("Thời gian kết thúc của bạn là: Ngày %d, Tháng %d, Năm %d, %dh:%2d'\n\n",dayFinish,monthFinish,yearFinish,hourFinish,minuteFinish);
-
+    System.out.printf("Thời gian kết thúc của bạn là: Ngày %d, Tháng %d, Năm %d, %dh:%2d'\n\n", dayFinish, monthFinish,
+        yearFinish, hourFinish, minuteFinish);
 
     scan.close();
-     // chuyển startTimeStr và finishTimeStr sang kiểu LocalDateTime
+    // chuyển startTimeStr và finishTimeStr sang kiểu LocalDateTime
 
-    
-    LocalDateTime startTime = LocalDateTime.of(LocalDate.of(year, month, day), LocalTime.of(hour, minute) );
-    LocalDateTime finishTime = LocalDateTime.of(LocalDate.of(yearFinish, monthFinish, dayFinish), LocalTime.of(hourFinish, minuteFinish));
+    LocalDateTime startTime = LocalDateTime.of(LocalDate.of(year, month, day), LocalTime.of(hour, minute));
+    LocalDateTime finishTime = LocalDateTime.of(LocalDate.of(yearFinish, monthFinish, dayFinish),
+        LocalTime.of(hourFinish, minuteFinish));
 
     Duration duration = Duration.between(startTime, finishTime);
-      
-    
-        // Tính và in ra thời gian chạy của bạn
-        System.out.printf("Thời gian chạy %s phút\n", duration.toMinutes());
-        // Tính và in ra vận tốc trung bình của bạn (km/h)
-        System.out.printf("Vận tốc trung bình %s km/h\n", (distant/(duration.toHours()) ));
-    
 
-
-
-
+    // Tính và in ra thời gian chạy của bạn
+    System.out.printf("Thời gian chạy %s phút\n", duration.toMinutes());
+    // Tính và in ra vận tốc trung bình của bạn (km/h)
+    System.out.printf("Vận tốc trung bình %s km/h\n", (distant / (duration.toHours())));
 
   }
 }
