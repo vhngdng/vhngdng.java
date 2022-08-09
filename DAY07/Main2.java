d28ecc6e8a94de84503ae97889f92bc755fa7c85
dung <vhngdng@gmail.com>
2022-08-09 14:32:16 +0700

    BTVN DAY07

--- DAY07/Main2.java
+++ DAY07/Main2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main2 {
         static Scanner scanner = new Scanner(System.in);
-        static User[] user = new User[8];               //Đây là mảng chung chứa tất cả các phần tử
+        static User[] user = new User[8]; // Đây là mảng chung chứa tất cả các phần tử
         static Student[] student = new Student[6];
         static Teacher[] teacher = new Teacher[2];
         static boolean online = true;
@@ -31,7 +31,7 @@ public class Main2 {
                                 "nguyenphutrong@gmail.com", "0364234820", "online", "dang di hoc");
 
                 for (int i = 0; i < 6; i++) { // chuyen student vao user array
-                        user[i] = (User) student[i];          //  using super class reference      
+                        user[i] = (User) student[i]; // using super class reference
                 }
 
                 teacher[0] = new Teacher("gv1", "nguyen xuan nam", GENDER.MALE, User.setBirthDay("01-01-1988"),
@@ -44,75 +44,71 @@ public class Main2 {
                         user[i + 6] = (User) teacher[i];
                 }
 
+                // Print info
+                printInfo();
 
+                // Tim theo keyword
+                findWithKeyWord();
 
-                //Print info
-                // printInfo();
+                // Sap xep theo ten
+                arrangeByArrayName();
 
-                //Tim theo keyword
-                //findWithKeyWord();
-
-                //Sap xep theo ten
-                // arrangeByArrayName();
-
-                //Sap xep theo tuoi
+                // Sap xep theo tuoi
                 arrangeByArrayAge();
         }
-        //print Info
+
+        // print Info
         public static void printInfo() {
-                for (User usr : user) {         
+                for (User usr : user) {
                         User.printInfo(usr);
                 }
         }
 
-        //Tim theo keyword
-        public static void findWithKeyWord () {
+        // Tim theo keyword
+        public static void findWithKeyWord() {
                 System.out.println("Hay nhap tu khoa");
                 Scanner scanner = new Scanner(System.in);
                 String keyword = scanner.nextLine();
-                for(User usr : user) {
-                        User.hasKeyWord(keyword, usr );
+                for (User usr : user) {
+                        User.hasKeyWord(keyword, usr);
                 }
                 scanner.close();
         }
-        
-        //Sap xep theo ten
+
+        // Sap xep theo ten
         public static void arrangeByArrayName() {
                 String userName[] = new String[8];
-                for (int i = 0 ; i < 8; i++) {
-                        userName[i] = user[i].name;     // Tao array String chua name cua cac user
+                for (int i = 0; i < 8; i++) {
+                        userName[i] = user[i].name; // Tao array String chua name cua cac user
                 }
-                Arrays.sort(userName);                  // Sap xep lai array String userName
-                for (int i = 0; i < 8; i++) {           //Loop 1, thu tu userName
-                        for (int j = 0; j < 8; j++) {   //Loop 2, user co name trung voi userName -> printInfo
-                                if (userName[i] == user[j].name ){
-                                System.out.println(user[j].name);
-                                User.printInfo(user[j]);
+                Arrays.sort(userName); // Sap xep lai array String userName
+                for (int i = 0; i < 8; i++) { // Loop 1, thu tu userName
+                        for (int j = 0; j < 8; j++) { // Loop 2, user co name trung voi userName -> printInfo
+                                if (userName[i] == user[j].name) {
+                                        System.out.println(user[j].name);
+                                        User.printInfo(user[j]);
                                 }
                         }
                 }
         }
 
         public static void arrangeByArrayAge() {
-                Integer age[] = new Integer[8];                 // Tao array Integer la tuoi cua cac user
-                for (int i = 0; i< 8 ; i++) {
-                        age[i] = User.findAge(user[i]);         
+                Integer age[] = new Integer[8]; // Tao array Integer la tuoi cua cac user
+                for (int i = 0; i < 8; i++) {
+                        age[i] = User.findAge(user[i]);
                 }
 
-                
-                Arrays.sort(age, Collections.reverseOrder());   // Sử dụng wrapper class Integer để  dùng reverseOrder
-                                                                //Sắp xếp theo thứ tự giảm dần
-                
+                Arrays.sort(age, Collections.reverseOrder()); // Sử dụng wrapper class Integer để dùng reverseOrder
+                                                              // Sắp xếp theo thứ tự giảm dần
 
-                for (Integer i: age) {                          // các phẩn tử của Array age = age of user
-                     for(User usr: user){
-                        if (i == User.findAge(usr)) {
-                                System.out.println("tuoi " + i);
-                                User.printInfo(usr);            
+                for (Integer i : age) { // các phẩn tử của Array age = age of user
+                        for (User usr : user) {
+                                if (i == User.findAge(usr)) {
+                                        System.out.println("tuoi " + i);
+                                        User.printInfo(usr);
+                                }
                         }
-                     }
                 }
-               
 
         }
 }
--- DAY07/Student.java
+++ DAY07/Student.java
@@ -26,7 +26,6 @@ public class Student extends User {
     @Override
     public String toString() {
         //Override method toString() 
-        ;
         String text = super.toString() + " | background: " + backGround + " | " + isOnline;
 
         return text;
